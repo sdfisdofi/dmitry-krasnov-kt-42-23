df396e11335fcef2e63526005d8ef7f002734c48
ДК <dk@example.ru>
2026-09-23 12:56:43 +0300

    add logging and WeatherForecast test request

--- dmitry-krasnov-kt-42-23/Controllers/WeatherForecastController.cs
+++ dmitry-krasnov-kt-42-23/Controllers/WeatherForecastController.cs
@@ -10,10 +10,16 @@ namespace dmitry_krasnov_kt_42_23.Controllers
         [
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         ];
+        private readonly ILogger<WeatherForecastController> _logger;
 
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogError("Method was called");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -21,6 +27,16 @@ namespace dmitry_krasnov_kt_42_23.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpPost(Name = "AddNewSummary")]
+        public string[] AddNewSummary(string newSummary)
+        {
+            _logger.LogError("New method was called");
+
+            var list = Summaries.ToList();
+            list.Add(newSummary);
+            return list.ToArray();
         }
     }
 }
